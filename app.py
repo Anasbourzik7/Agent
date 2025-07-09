db6690d0d86d365b9442bca0b2c843c0fbf4dd50
Anas BOURZIK <163159759+Anasbourzik7@users.noreply.github.com>
2025-07-09 10:22:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,12 +51,23 @@ with st.form("oracle_conn_form"):
 
     st.markdown("<label style='color:black;'>Mot de passe</label>", unsafe_allow_html=True)
     st.text_input(label="", type="password", disabled=True)
-    with col2:
+    st.markdown("<label style='color:black;'>Hôte</label>", unsafe_allow_html=True)
+    st.text_input(label="", placeholder="ex: 127.0.0.1", disabled=True)
+
+    st.markdown("<label style='color:black;'>Port</label>", unsafe_allow_html=True)
+    st.text_input(label="", placeholder="ex: 1521", disabled=True)
+
+    st.markdown("<label style='color:black;'>Utilisateur</label>", unsafe_allow_html=True)
+    st.text_input(label="", disabled=True)
+
+    st.markdown("<label style='color:black;'>Mot de passe</label>", unsafe_allow_html=True)
+    st.text_input(label="", type="password", disabled=True)
+
+with col2:
         st.text_input("Service Name ou SID", placeholder="ex: ORCL", disabled=True)
         st.checkbox("Utiliser un SID", value=False, disabled=True)
 
     st.form_submit_button("Se connecter", disabled=True)
-
 # === Bouton de statut connexion simulée (désactivé)
 st.markdown("""
     <div style="text-align:right; padding:10px 0;">
